0c8ed2774969936f79683ff0004d1cded10a1383
David Kerr <kerrky@gmail.com>
2024-04-05 09:21:20 -0400

    Updated lecture

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ import ipdb
 from flask import Flask, make_response
 from flask_migrate import Migrate
 
-from models import db, Hotel, Customer
+from models import db, Hotel, Customer, Review
 
 app = Flask(__name__)
 
@@ -22,9 +22,32 @@ db.init_app(app)
 
 @app.route('/hotels')
 def all_hotels():
-    hotels = Hotel.query.all()
+    hotels = Hotel.query.all(only=('id', 'name'))
     hotel_list_with_dictionaries = [hotel.to_dict() for hotel in hotels]
-    return make_response(hotel_list_with_dictionaries)
+    return make_response(hotel_list_with_dictionaries, 200)
+
+@app.route('/hotels/<int:id>')
+def hotel_by_id(id):
+    hotel = db.session.get(Hotel, id)
+    if hotel:
+        body = hotel.to_dict(rules=('-reviews.hotel', '-reviews.customer.reviews'))
+        status = 200
+    else:
+        body = {"message": f"Hotel {id} is not found."}
+        status = 404
+    return make_response(body, status)
+
+@app.route('/customers')
+def all_customers():
+    customers = Customer.query.all()
+    customer_list_with_dict = [customer.to_dict(only=('id', 'first_name', 'last_name')) for customer in customers]
+    return make_response(customer_list_with_dict, 200)
+
+@app.route('/reviews')
+def all_reviews():
+    reviews = Review.query.all()
+    review_list_with_dict = [review.to_dict(rules=('-hotel.reviews', '-customer.reviews')) for review in reviews]
+    return make_response(review_list_with_dict, 200)
 
 if __name__ == "__main__":
     app.run(port=7777, debug=True)
--- server/models.py
+++ server/models.py
@@ -13,12 +13,42 @@ db = SQLAlchemy(metadata=metadata)
 class Hotel(db.Model, SerializerMixin):
     __tablename__ = 'hotels'
 
+    # serialize_rules = ('-reviews.hotel', )
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+
+    reviews = db.relationship('Review', back_populates='hotel', cascade='all')
+
+    customers = association_proxy('reviews', 'customer', creator = lambda c: Review(customer = c))
 
 class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
 
+    # serialize_rules = ('-reviews.customer', )
+
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)
+
+    reviews = db.relationship('Review', back_populates='customer', cascade='all')
+
+    hotels = association_proxy('reviews', 'hotel', creator = lambda h: Review(hotel = h))
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    # serialize_rules = ('-hotel.reviews', '-customer.reviews')
+
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer)
+    text = db.Column(db.String)
+    
+    hotel_id = db.Column(db.Integer, db.ForeignKey("hotels.id"))
+    customer_id = db.Column(db.Integer, db.ForeignKey("customers.id"))
+
+    hotel = db.relationship('Hotel', back_populates='reviews')
+    customer = db.relationship('Customer', back_populates='reviews')
+
+
+
